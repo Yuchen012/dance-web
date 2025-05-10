b0b5afb1dd369e386bd715c11c1d3f83296f837f
Yuchen012 <ncstarry0514k@gmail.com>
2025-05-11 03:01:47 +0900

    料金表アップデート

--- app/page.tsx
+++ app/page.tsx
@@ -48,44 +48,58 @@ export default function Home() {
 
       {/* 最初に見えるコンテンツ */}
       <section className="intro-content">
-        <h2>スタジオの説明</h2>
-        <p>あああああああああああああああああああああああああああああああああああああああああああああ</p>
-        <p>あああああああああああああああああああああああああああああああああああああああああああああ</p>
+        <h2>ジャズダンス・モダンバレエ入門！</h2>
+        <p>音楽にのって楽しく身体を動かしながら</p>
+        <p>体力・筋力・柔軟性・バランス感覚の向上を目指します。</p>
+        <p>ピラティスでは美しい姿勢、しっかりした体幹を！</p>
       </section>
 
 
 
       <main className="main-content">
       <section className="content-box content-one">
-  <div className="class-fee-container three-cards-row">
-    <div className="price-card unified-card">
-      <h3 className="class-title">小学校クラス</h3>
-      <p className="class-description">
-        運動習慣は子どもにとって大切です。<br />
-        楽しく動きながら、運動能力向上や良い姿勢を目指します。
-      </p>
-      <div className="class-price">月4回／6,500円（60分）</div>
+  <div className="class-fee-wrapper">
+    <div className="class-fee-container three-cards-row">
+      <div className="price-card unified-card">
+        <h3 className="class-title">小学校クラス</h3>
+        <p className="class-description">
+          運動習慣は子どもにとって大切です。<br />
+          楽しく動きながら、運動能力向上や良い姿勢を目指します。
+        </p>
+        <div className="class-price">月4回／6,500円（60分）</div>
+      </div>
+
+      <div className="price-card unified-card">
+        <h3 className="class-title">親子クラス（幼児・小学生）</h3>
+        <p className="class-description">
+          50分ですが、ストレッチ・ダンスバレエの基礎・踊りを内容豊富に。<br />
+          お母さんにとっても良い運動になります。
+        </p>
+        <div className="class-price">月4回／5,000円（50分・1人）</div>
+      </div>
+
+      <div className="price-card unified-card">
+        <h3 className="class-title">おとなクラス</h3>
+        <p className="class-description">
+          学生からシニアまでピラティス・ダンス・バレエで<br />
+          美しい姿勢や体力づくりを目指します。
+        </p>
+        <div className="class-price">月4回／6,500円～（90分）</div>
+      </div>
     </div>
 
-    <div className="price-card unified-card">
-      <h3 className="class-title">親子クラス（幼児・小学生）</h3>
-      <p className="class-description">
-        50分ですが、ストレッチ・ダンスバレエの基礎・踊りを内容豊富に。<br />
-        お母さんにとっても良い運動になります。
-      </p>
-      <div className="class-price">月4回／5,000円（50分・1人）</div>
-    </div>
-
-    <div className="price-card unified-card">
-      <h3 className="class-title">おとなクラス</h3>
-      <p className="class-description">
-        学生からシニアまでピラティス・ダンス・バレエで<br />
-        美しい姿勢や体力づくりを目指します。
-      </p>
-      <div className="class-price">月4回／6,500円～（90分）</div>
+    
+    <div className="class-notes">
+      <ul>
+        <li>レッスン料の他に、ひとりにつき入会金 5,000円が必要となります。（※全て税込表示となります）</li>
+        <li>小学生・親子クラスは、ストレッチ・ダンス・バレエ中心となります（ピラティスもあります）</li>
+        <li>幼児のみのクラスもございますので、お気軽にお問い合わせください。</li>
+        <li>火・水・金・日曜日にレッスンがあります。レッスン時間などはお問い合わせください。</li>
+      </ul>
     </div>
   </div>
 </section>
+
 
 
 
