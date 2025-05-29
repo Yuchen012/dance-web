1f4cc4b34806ed175fa0d6a4a9bb98884d3ef266
Yuchen012 <ncstarry0514k@gmail.com>
2025-05-29 16:36:27 +0900

    タイトル設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Studio Anne | 山梨県甲府市のスタジオ",
   description: "ジャズダンス・ピラティス・モダンバレエ入門！音楽にのって楽しく身体を動かしながら体力・筋力・柔軟性・バランス感覚の向上を目指します。ピラティスでは美しい姿勢、しっかりした体幹を！",
+  openGraph: {
+    title: "Studio Anne | 山梨県甲府市のスタジオ",
+    description: "ジャズダンス・ピラティス・モダンバレエ入門！音楽にのって楽しく身体を動かしながら体力・筋力・柔軟性・バランス感覚の向上を目指します。ピラティスでは美しい姿勢、しっかりした体幹を！",
+    type: "website",
+    url: "https://studio-anne.net",
+  },
 };
 
 export default function RootLayout({
